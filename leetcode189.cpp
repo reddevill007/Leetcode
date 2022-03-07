2c196b7a171d2c50dd487e36eb74f5232d89a8ae
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2022-03-07 07:06:15 +0530

    Create leetcode189.cpp

---
+++ leetcode189.cpp
@@ -0,0 +1,33 @@
+/*
+  Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+
+
+  Example 1:
+
+  Input: nums = [1,2,3,4,5,6,7], k = 3
+  Output: [5,6,7,1,2,3,4]
+  Explanation:
+  rotate 1 steps to the right: [7,1,2,3,4,5,6]
+  rotate 2 steps to the right: [6,7,1,2,3,4,5]
+  rotate 3 steps to the right: [5,6,7,1,2,3,4]
+  Example 2:
+
+  Input: nums = [-1,-100,3,99], k = 2
+  Output: [3,99,-1,-100]
+  Explanation: 
+  rotate 1 steps to the right: [99,-1,-100,3]
+  rotate 2 steps to the right: [3,99,-1,-100]
+*/
+
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        if (!nums.empty()) {
+            k %= nums.size();
+            reverse(nums.begin(), nums.begin() + nums.size() - k);
+            reverse(nums.begin() + nums.size() - k, nums.end());
+            reverse(nums.begin(), nums.end());
+        }
+    }
+};
