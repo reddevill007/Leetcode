e6f414509a327c4e802d0b3c086d00dec3bb2200
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2022-03-07 07:10:23 +0530

    Create leetcode347.cpp

---
+++ leetcode347.cpp
@@ -0,0 +1,39 @@
+/*
+    Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+    
+
+    Example 1:
+
+    Input: nums = [1,1,1,2,2,3], k = 2
+    Output: [1,2]
+    Example 2:
+
+    Input: nums = [1], k = 1
+    Output: [1]
+*/
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> cnt;
+		for (int x : nums) 	cnt[x] ++;
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+		for (auto &x : cnt) {
+			if (q.size() < k)
+				q.push(make_pair(x.second, x.first));
+			else {
+				if (q.top().first < x.second) {
+					q.pop();
+					q.push(make_pair(x.second, x.first));
+				}
+			}
+		}
+		vector<int> ans;
+		while (!q.empty()) {
+			ans.push_back(q.top().second);
+			q.pop();
+		}
+		return ans;
+    }
+};
