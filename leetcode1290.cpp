3a26dbc09f80805eba0ecb8a1d142c5db8365e15
Saurabh Pandey <74395114+reddevill007@users.noreply.github.com>
2022-03-23 10:37:23 +0530

    Create leetcode1290.cpp

---
+++ leetcode1290.cpp
@@ -0,0 +1,45 @@
+/*
+  1290. Convert Binary Number in a Linked List to Integer
+
+  Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+
+  Return the decimal value of the number in the linked list.
+
+  Example 1:
+    Input: head = [1,0,1]
+    Output: 5
+    Explanation: (101) in base 2 = (5) in base 10
+    
+  Example 2:
+    Input: head = [0]
+    Output: 0
+
+
+  Constraints:
+    The Linked List is not empty.
+    Number of nodes will not exceed 30.
+    Each node's value is either 0 or 1.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int ans = 0;
+        while(head != NULL) {
+            ans *= 2;
+            ans += (head->val);
+            head = head->next;
+        }
+        return ans;
+    }
+};
